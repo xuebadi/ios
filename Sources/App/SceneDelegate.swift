485a0890ca41697c061813c970aca6f11af84db2
学霸帝 <jvhv5bqpfd@privaterelay.appleid.com>
2026-05-13 08:38:54 +0800

    Add Sources/App/SceneDelegate.swift

---
+++ Sources/App/SceneDelegate.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+
+        // Default: use local embedded model (Qwen2.5-VL-3B-Instruct via MNN)
+        let chatVC = ChatViewController()
+        let navController = UINavigationController(rootViewController: chatVC)
+
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+    func sceneWillResignActive(_ scene: UIScene) {}
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+    func sceneDidEnterBackground(_ scene: UIScene) {}
+}
